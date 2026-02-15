80f6bedecf15c65fb3fab535ea4d90c31def7b76
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-15 15:55:33 +0330

    add test registar code

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 
@@ -18,6 +19,23 @@ def user_login(request):
 
 
 def user_register(request):
+    context = {"errors": []}
+    if request.user.is_authenticated:
+           return redirect('/')
+    
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        passwoed2 = request.POST.get('password2')
+        if password1 != passwoed2:
+            context['errors'].append('passwords are not same')
+            return render(request, "accounts/Register.html", context)
+        user = User.objects.create(username=username, password=password1, email=email)
+        login(request, user)
+        return redirect('/')
+
     return render(request, "accounts/Register.html", {})
 
 def user_logout(request):
