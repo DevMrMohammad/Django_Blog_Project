6174e67577b3055b8199d49bf6b9805aaa4dca5e
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-16 13:28:26 +0330

    add a comment for object author

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# object for author articles ---------------------------.----------------------.
+
+# cascade
+
+# set null
+
+# protect
+
+# set default
+
+# ------------------------------------------------------.----------------------.
+
+
+
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete =models.CASCADE, )
     # user and CADCADE = if delete user : all data for in user deleted 
