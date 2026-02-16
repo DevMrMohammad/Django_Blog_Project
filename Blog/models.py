966b3b849cb529f7d33b8742127b473a6d8b18cb
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-16 11:23:42 +0330

    add auther for articles and create new migration and ron make migratin and migration

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
+    auther = models.ForeignKey(User, on_delete =models.CASCADE, )
+    # user and CADCADE = if delete user : all data for in user deleted 
     title = models.CharField(max_length=70)
     body = models.TextField()
     image = models.ImageField(upload_to="images/articles")
