7912e3aebb0244114f85cee6f8ec52d630faa14a
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-16 10:10:09 +0330

    add mdoels and install pollow library and add model to file admin

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/articles")
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f"{self.title} - {self.body[:30]}"
