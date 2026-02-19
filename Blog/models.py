1f758c676c0e77633ac6593d843f3e74c68a6e7f
DevMrMohammad <mohammadsalarkarimabadi909@gmail.com>
2026-02-19 20:42:17 +0330

    create categore model and registar to admin file and ManytoMany connect for category and articles

--- Blog/models.py
+++ Blog/models.py
@@ -16,6 +16,16 @@ from django.utils import timezone
 
 
 
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+
+    def  __str__(self):
+        return self.title
+
+
 class Article(models.Model):
 
     CHOICES = (
@@ -23,6 +33,7 @@ class Article(models.Model):
         ('b', 'django'),
     )
     author = models.ForeignKey(User, on_delete =models.CASCADE)
+    category = models.ManyToManyField(Category)
     # help_text = enter a text for help write at more fild 
     # UNIQE = because not replay title or mode data 
     # CHOICES = because select betwin two option for fild
